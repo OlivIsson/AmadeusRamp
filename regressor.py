6e07fc993bbabc6d20d57c2909ff61521d7cbd34
OlivIsson <olivier.isson@gmail.com>
2015-07-04 12:01:10 +0200

    Update regressor.py

--- regressor.py
+++ regressor.py
@@ -12,10 +12,22 @@ import xgboost as xgb
 
 class Regressor(BaseEstimator):
     def __init__(self):
-        self.clf = xgb.XGBRegressor(max_depth=17, n_estimators=1673, learning_rate=0.04)
+        self.clf = GradientBoostingRegressor( n_estimators = 1950 , max_depth = 9 , max_features = 27)
+        #self.clf = Pipeline([('scaler', StandardScaler()),
+        #                     ("RF", RandomForestRegressor(n_estimators=50, max_depth = 10))])
+        self.clf1 = xgb.XGBRegressor(max_depth=17, n_estimators=1000, learning_rate=0.05)
+        #self.clf1 = Pipeline([('scaler', StandardScaler()),
+                                  #("GB",GradientBoostingRegressor(n_estimators = 500, max_depth=6))])
+        #self.clf2 = Pipeline([('scaler', StandardScaler()),
+        #                      ("LR",LinearRegression())])
 
     def fit(self, X, y):
-        self.clf.fit(X, y)
+        self.clf.fit(X, y.reshape((y.shape[0],)))
+        self.clf1.fit(X, y.reshape((y.shape[0],)))
+        #self.clf2.fit(X, y.reshape((y.shape[0],)))
+        
 
     def predict(self, X):
-        return self.clf.predict(X)
+
+        #return sum(list_clf)/float(len(list_clf))
+        return self.clf.predict(X) * 0.52 + self.clf1.predict(X) * 0.48
