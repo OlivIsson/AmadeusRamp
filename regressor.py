809ac1d7ed69e34cbab5f463a6d8a486200e5fe8
OlivIsson <olivier.isson@gmail.com>
2015-07-04 14:16:11 +0200

    Update regressor.py

--- regressor.py
+++ regressor.py
@@ -10,24 +10,21 @@ from sklearn import neighbors
 from sklearn.decomposition import PCA
 import xgboost as xgb
 
+
 class Regressor(BaseEstimator):
     def __init__(self):
-        self.clf = GradientBoostingRegressor( n_estimators = 1950 , max_depth = 9 , max_features = 27)
-        #self.clf = Pipeline([('scaler', StandardScaler()),
-        #                     ("RF", RandomForestRegressor(n_estimators=50, max_depth = 10))])
+        self.clf0 = GradientBoostingRegressor( n_estimators = 2000 , max_depth = 9 , max_features = 27)
         self.clf1 = xgb.XGBRegressor(max_depth=17, n_estimators=1000, learning_rate=0.05)
-        #self.clf1 = Pipeline([('scaler', StandardScaler()),
-                                  #("GB",GradientBoostingRegressor(n_estimators = 500, max_depth=6))])
-        #self.clf2 = Pipeline([('scaler', StandardScaler()),
-        #                      ("LR",LinearRegression())])
-
-    def fit(self, X, y):
-        self.clf.fit(X, y.reshape((y.shape[0],)))
-        self.clf1.fit(X, y.reshape((y.shape[0],)))
-        #self.clf2.fit(X, y.reshape((y.shape[0],)))
+        self.clf2 = xgb.XGBRegressor(max_depth=17, n_estimators=1000, learning_rate=0.04)
         
 
-    def predict(self, X):
+    def fit(self, X, y):
+        self.clf0.fit(X[:,0:102], y)
+        self.clf1.fit(X[:,0:102], y)
+        self.clf2.fit(X[:,102:], y)
 
+    def predict(self, X):
+        #list_clf=[self.clf0.predict(X[:,0:102]),self.clf1.predict(X[:,102:])]
+        #list_clf=[self.clf0.predict(X[:,0:102])]
         #return sum(list_clf)/float(len(list_clf))
-        return self.clf.predict(X) * 0.6 + self.clf1.predict(X) * 0.4
+        return self.clf0.predict(X) * 0.41 + self.clf1.predict(X) * 0.39 +  self.clf2.predict(X) * 0.2
