a66c2da21dc0a11c486db478a332f35714d237ba
OlivIsson <olivier.isson@gmail.com>
2015-07-04 11:24:06 +0200

    Update regressor.py

--- regressor.py
+++ regressor.py
@@ -12,7 +12,7 @@ import xgboost as xgb
 
 class Regressor(BaseEstimator):
     def __init__(self):
-        self.clf = xgb.XGBRegressor(max_depth=17, n_estimators=1800, learning_rate=0.05)
+        self.clf = xgb.XGBRegressor(max_depth=17, n_estimators=1300, learning_rate=0.05)
 
     def fit(self, X, y):
         self.clf.fit(X, y)
