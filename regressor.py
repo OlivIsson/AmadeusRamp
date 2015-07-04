cfd9d1d06e25ecb95b42c65df5aa1d8209cdfe61
OlivIsson <olivier.isson@gmail.com>
2015-07-04 08:37:37 +0200

    Update regressor.py

--- regressor.py
+++ regressor.py
@@ -8,16 +8,14 @@ from sklearn.base import BaseEstimator
 from sklearn.decomposition import KernelPCA
 from sklearn import neighbors
 from sklearn.decomposition import PCA
+import xgboost as xgb
 
 class Regressor(BaseEstimator):
     def __init__(self):
-        self.clf0 = RandomForestRegressor(n_estimators=300, max_depth=50, max_features=20)
-        self.clf = GradientBoostingRegressor( n_estimators = 1500 , max_depth = 7 , max_features = 15)
+        self.clf = xgb.XGBRegressor(max_depth=17, n_estimators=1000, learning_rate=0.05)
 
     def fit(self, X, y):
-        self.clf0.fit(X[:,0:102], y)
-        self.clf.fit(X[:,102:], y)
+        self.clf.fit(X, y)
 
     def predict(self, X):
-        list_clf=[self.clf0.predict(X[:,0:102]),self.clf.predict(X[:,102:])]
-        return sum(list_clf)/float(len(list_clf))
+        return self.clf.predict(X)
