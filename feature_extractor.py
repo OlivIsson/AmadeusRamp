89fa2c3c761bf925ca6f6a90ddc11f9ac147a20c
OlivIsson <olivier.isson@gmail.com>
2015-07-04 16:33:37 +0200

    Update feature_extractor.py

--- feature_extractor.py
+++ feature_extractor.py
@@ -1,10 +1,3 @@
-# Mixed Short Long
-
-import pandas as pd 
-import os
-import numpy as np
-
-
 class FeatureExtractor(object):
     def __init__(self):
         pass
@@ -13,49 +6,57 @@ class FeatureExtractor(object):
         pass
 
     def transform(self, X_df):
-        X_encoded = X_df # short
+        X_encoded = X_df
+        
         X_encoded = X_encoded.join(pd.get_dummies(X_encoded['Departure'], prefix='d'))
         X_encoded = X_encoded.join(pd.get_dummies(X_encoded['Arrival'], prefix='a'))
-        X_encoded = X_encoded.drop('Departure', axis=1)
-        X_encoded = X_encoded.drop('Arrival', axis=1)
-    
+       
+
+        # following http://stackoverflow.com/questions/16453644/regression-with-date-variable-using-scikit-learn
         X_encoded['DateOfDeparture'] = pd.to_datetime(X_encoded['DateOfDeparture'])
         X_encoded['year'] = X_encoded['DateOfDeparture'].dt.year
+        X_encoded['month'] = X_encoded['DateOfDeparture'].dt.month
+        X_encoded['quarter'] = X_encoded['DateOfDeparture'].dt.quarter
+        X_encoded['day'] = X_encoded['DateOfDeparture'].dt.day
         X_encoded['weekday'] = X_encoded['DateOfDeparture'].dt.weekday
         X_encoded['week'] = X_encoded['DateOfDeparture'].dt.week
+        X_encoded['n_days'] = X_encoded['DateOfDeparture'].apply(lambda date: (date - pd.to_datetime("1970-01-01")).days)
+
         X_encoded = X_encoded.join(pd.get_dummies(X_encoded['year'], prefix='y'))
+        X_encoded = X_encoded.join(pd.get_dummies(X_encoded['month'], prefix='m'))
+        X_encoded = X_encoded.join(pd.get_dummies(X_encoded['day'], prefix='d'))
         X_encoded = X_encoded.join(pd.get_dummies(X_encoded['weekday'], prefix='wd'))
         X_encoded = X_encoded.join(pd.get_dummies(X_encoded['week'], prefix='w'))
-        X_encoded = X_encoded.drop('weekday', axis=1)
-        X_encoded = X_encoded.drop('week', axis=1)
+        
+        #fares = pd.read_csv("AirFares2012Q1to2013Q2.csv",sep=';')
+        
+        path = os.path.dirname(__file__)
+                #data_weather = pd.read_csv(os.path.join(path, "data_weather.csv"))
+        fares = pd.read_csv(os.path.join(path,"AirFares2012Q1to2013Q2.csv"), sep = ';')
+                
+        X_fares = fares [['ORIGIN', 'DEST', 'Quarter', 'TotalPax','TotalFare', 'AverageFare', 'Year']]
+        X_encoded = X_encoded.merge(X_fares, how='left',
+                    left_on=['Departure', 'Arrival','quarter','year'], 
+                    right_on=['ORIGIN','DEST','Quarter','Year'], sort=False)
+        
+        X_encoded = X_encoded.drop('DateOfDeparture', axis=1)
+        X_encoded = X_encoded.drop('Departure', axis=1)
+        X_encoded = X_encoded.drop('Arrival', axis=1)
+        
+        X_encoded = X_encoded.drop('ORIGIN', axis=1)
+        X_encoded = X_encoded.drop('DEST', axis=1)
+        X_encoded = X_encoded.drop('Quarter', axis=1)
+        X_encoded = X_encoded.drop('Year', axis=1)
         X_encoded = X_encoded.drop('year', axis=1)
-        X_encoded = X_encoded.drop('std_wtd', axis=1)
-        X_encoded = X_encoded.drop('WeeksToDeparture', axis=1)        
-        X_encoded = X_encoded.drop('DateOfDeparture', axis=1)     
+        X_encoded = X_encoded.drop('quarter', axis=1)
+        X_encoded = X_encoded.drop('TotalFare', axis=1)
+        X_encoded = X_encoded.drop('TotalPax', axis=1)
         
-        data_encoded = X_df # Long
-        data_encoded = data_encoded.join(pd.get_dummies(data_encoded['Departure'], prefix='d'))
-        data_encoded = data_encoded.join(pd.get_dummies(data_encoded['Arrival'], prefix='a'))
-        data_encoded = data_encoded.drop('Departure', axis=1)
-        data_encoded = data_encoded.drop('Arrival', axis=1)
-
-
-        data_encoded['DateOfDeparture'] = pd.to_datetime(data_encoded['DateOfDeparture'])
-        data_encoded['year'] = data_encoded['DateOfDeparture'].dt.year
-        data_encoded['month'] = data_encoded['DateOfDeparture'].dt.month
-        data_encoded['day'] = data_encoded['DateOfDeparture'].dt.day
-        data_encoded['weekday'] = data_encoded['DateOfDeparture'].dt.weekday
-        data_encoded['week'] = data_encoded['DateOfDeparture'].dt.week
-        data_encoded['n_days'] = data_encoded['DateOfDeparture'].apply(lambda date: (date - pd.to_datetime("1970-01-01")).days)
-
-        data_encoded = data_encoded.join(pd.get_dummies(data_encoded['year'], prefix='y'))
-        data_encoded = data_encoded.join(pd.get_dummies(data_encoded['month'], prefix='m'))
-        data_encoded = data_encoded.join(pd.get_dummies(data_encoded['day'], prefix='d'))
-        data_encoded = data_encoded.join(pd.get_dummies(data_encoded['weekday'], prefix='wd'))
-        data_encoded = data_encoded.join(pd.get_dummies(data_encoded['week'], prefix='w'))
-
-        data_encoded = data_encoded.drop(['DateOfDeparture'], axis=1)
-
+        aaa=X_encoded['AverageFare']
+        ii=np.isnan(X_encoded['AverageFare'])
+        rr=np.where(ii)
+        jj=np.where(ii == True)[0]
+        X_encoded['AverageFare'][jj]=0.0
         
-        X_array = np.concatenate((X_encoded.values, data_encoded.values), axis=1)
+        X_array = X_encoded.values
         return X_array
